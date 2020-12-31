cbf9c8c382a51bd9bfa8d3fcbb11e617903e56f4
Devish Baidawar Chhetri <devishbaidawar@gmail.com>
2020-12-31 17:38:08 +0545

    Added Songs (ChillHop)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles/app.scss';
 import Song from './components/Song';
 import Player from './components/Player';
+import SongLists from './SongsList';
 
 function App () {
   return (
---
+++ src/SongsList.js
@@ -0,0 +1,62 @@
+import { v4 as uuid } from 'uuid';
+
+function chillHopMusics () {
+  return [
+    {
+      id: uuid(),
+      name: "Wake Up",
+      artist: "Miscél",
+      cover: "https://chillhop.com/wp-content/uploads/2020/07/edaf1202cf33c7a2a122b7494a7e4d3e643af45f-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9320",
+      color: [ '#DE7497', '#A64067' ],
+      active: true
+    },
+    {
+      id: uuid(),
+      name: "Mirage",
+      artist: "Nymano, j'san",
+      cover: "https://chillhop.com/wp-content/uploads/2020/09/09fb436604242df99f84b9f359acb046e40d2e9e-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10136",
+      color: [ '#5A4F8B', '#966B8B' ],
+      active: false
+    },
+    {
+      id: uuid(),
+      name: "Calm",
+      artist: "ØDYSSEE, Florent Garcia",
+      cover: "https://chillhop.com/wp-content/uploads/2020/06/d9d8ae69141cb7838e3c53bbf393c6218285e384-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9285",
+      color: [ '#FFC8BD', '#F9C8BE' ],
+      active: false
+    },
+    {
+      id: uuid(),
+      name: "Wake Up",
+      artist: "Miscél",
+      cover: "https://chillhop.com/wp-content/uploads/2020/07/edaf1202cf33c7a2a122b7494a7e4d3e643af45f-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=",
+      color: [ '#DE7497', '#A64067' ],
+      active: false
+    },
+    {
+      id: uuid(),
+      name: "Girls Just Wanna Have Sun",
+      artist: "SAINT WKND",
+      cover: "https://chillhop.com/wp-content/uploads/2020/05/260a909fca701bbb7593f5f8f9cff9508cca2856-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=7930",
+      color: [ '#7B236D', '#D3AB32' ],
+      active: false
+    },
+    {
+      id: uuid(),
+      name: "hau Nalu",
+      artist: "FloFilz, Kostral One",
+      cover: "https://chillhop.com/wp-content/uploads/2020/10/0e5bb63f838ff92eeac142aae944e9f678df13c9-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10334",
+      color: [ '#4A5E91', '#AC3338' ],
+      active: false
+    }
+  ];
+}
+
+export default chillHopMusics;
