145468f6e45323254ecb26806bef331f69685447
Devish Baidawar Chhetri <devishbaidawar@gmail.com>
2020-12-31 18:50:40 +0545

    Rendered Song to a player

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/app.scss';
 import Song from './components/Song';
 import Player from './components/Player';
 import SongLists from './SongsList';
 
 function App () {
+  const [ songs, setSongs ] = useState( SongLists );
+  const [ currentSong, setCurrentSong ] = useState( songs[ 2 ] );
+  const [ isPlaying, setIsPlaying ] = useState( false );
   return (
     <div className="App">
-      <h1 style={{textAlign:"center"}}>Music Player</h1>
-      <Song />
-      <Player />
+      <Song currentSong={ currentSong } />
+      <Player currentSong={ currentSong } isPlaying={ isPlaying } setIsPlaying={ setIsPlaying } />
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,19 +1,82 @@
-import React from 'react';
-import { FaStepBackward, FaPlay, FaStepForward } from 'react-icons/fa';
+import React, { useRef, useState } from 'react';
+import { FaStepBackward, FaPlay, FaStepForward, FaPause } from 'react-icons/fa';
 
-const Player = () => {
+const Player = ( { currentSong, isPlaying, setIsPlaying } ) => {
+  /* ----- Audio Ref Begins ----- */
+  const audioRef = useRef( null );
+  /* ----- Audio Ref Ends ----- */
+
+  /* ----- Play Song Event Handlers Begins -----*/
+  const playSongHandler = () => {
+    if ( isPlaying ) {
+      audioRef.current.pause();
+      setIsPlaying( !isPlaying );
+    } else {
+      audioRef.current.play();
+      setIsPlaying( !isPlaying );
+    }
+  }
+  /* ----- Play Song Event Handlers Ends -----*/
+
+  /* ----- Song Info State Begins ----- */
+  const [ songInfo, setSongInfo ] = useState( {
+    currentTime: 0,
+    fullTime: 0
+  } );
+  /* ----- Song Info State Ends ----- */
+
+  /* ----- Updating Time Begins ----- */
+  const timeUpdateHandler = ( e ) => {
+    // console.log( e );
+    const currentTime = e.target.currentTime;
+    const fullTime = e.target.duration;
+    setSongInfo( { ...songInfo, currentTime, fullTime } );
+  }
+  /* ----- Updating Time Ends ----- */
+
+  /* ----- Time Formatting Begins ----- */
+  const getTime = ( time ) => {
+    return (
+      Math.floor( time / 60 ) + ':' + ( "0" + Math.floor( time % 60 ) ).slice( -2 )
+    );
+  };
+  /* ----- Time Formatting Ends ----- */
+
+  /* ----- Time Range Handler Begins ----- */
+  const timeRangeHandler = ( e ) => {
+    // console.log( e.target.value )
+    audioRef.current.currentTime = e.target.value;
+    setSongInfo( { ...songInfo, currentTime: e.target.value } )
+  };
+  /* ----- Time Range Handler Ends ----- */
   return (
     <div className="player-container">
       <div className="time-control">
-        <p>Start Time</p>
-        <input type="range" />
-        <p>End Time</p>
+        <p><strong>{ getTime( songInfo.currentTime ) }</strong></p>
+        <input
+          type="range"
+          min={ 0 }
+          max={ songInfo.fullTime }
+          value={ songInfo.currentTime }
+          onChange={ timeRangeHandler }
+        />
+        <p><strong>{ getTime( songInfo.fullTime ) }</strong></p>
       </div>
       <div className="play-control">
         <FaStepBackward className="skip-back" size="28px" />
-        <FaPlay className="play" size="28px" />
+        {
+          isPlaying
+            ? <FaPause className="pause" size="28px" onClick={ playSongHandler } />
+            : <FaPlay className="play" size="28px" onClick={ playSongHandler } />
+        }
         <FaStepForward className="skip-forward" size="28px" />
       </div>
+      <audio
+        src={ currentSong.audio }
+        ref={ audioRef }
+        onTimeUpdate={ timeUpdateHandler }
+        onLoadedMetadata={ timeUpdateHandler }
+      ></audio>
     </div>
   )
 }
