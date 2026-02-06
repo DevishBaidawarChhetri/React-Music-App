ce5b5384ae42082a26a4d5c22a68e89605e698a1
DevvvIshhh <devishbaidawar@gmail.com>
2026-02-06 12:25:33 +1100

    music url updated and fixes

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,28 +1,33 @@
-import React from 'react';
+import React from "react";
+import songImg from "./library/song.png";
 
-const Song = ( { currentSong, isPlaying, toggleLibrary } ) => {
+const Song = ({ currentSong, isPlaying, toggleLibrary }) => {
+  const addDefaultSrc = (e) => {
+    e.target.src = songImg;
+  };
+
   return (
-    <div className="song-container" onClick={ toggleLibrary }>
-      {/* {console.log( isPlaying ) } */ }
+    <div className="song-container" onClick={toggleLibrary}>
+      {/* {console.log( isPlaying ) } */}
       <img
-        className={ isPlaying ? "rotateImage" : "" }
-        src={ currentSong.cover }
-        alt={ currentSong.name }
-        style={ {
-          borderWidth: '5px',
-          borderStyle: 'solid',
-          borderColor:
-            `${ currentSong.color[ 1 ] } 
-            ${ currentSong.color[ 1 ] }
-            ${ currentSong.color[ 0 ] } 
-            ${ currentSong.color[ 0 ] } `,
-          padding: '0.3rem'
-        } }
+        className={isPlaying ? "rotateImage" : ""}
+        src={currentSong.cover}
+        alt={currentSong.name}
+        style={{
+          borderWidth: "5px",
+          borderStyle: "solid",
+          borderColor: `${currentSong.color[1]} 
+            ${currentSong.color[1]}
+            ${currentSong.color[0]} 
+            ${currentSong.color[0]} `,
+          padding: "0.3rem",
+        }}
+        onError={addDefaultSrc}
       />
-      <h2>{ currentSong.name }</h2>
-      <h3>{ currentSong.artist }</h3>
+      <h2>{currentSong.name}</h2>
+      <h3>{currentSong.artist}</h3>
     </div>
-  )
-}
+  );
+};
 
 export default Song;
